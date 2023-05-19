1eaeac83a9095a2a15d6c2c1f1a2e62d2a9ff791
Christian Areas <me@areas.me>
2023-05-19 15:32:12 -0500

    Updated link to OpenAPI spec.

--- app/docs/api/page.tsx
+++ app/docs/api/page.tsx
@@ -8,7 +8,7 @@ export default function ApiReference() {
 	return (
 		<API
 			basePath="/docs/api"
-			apiDescriptionUrl="https://raw.githubusercontent.com/areas-apis/resume-api/main/docs/spec/_versions/resume-api-0.1.0.yaml"
+			apiDescriptionUrl="https://raw.githubusercontent.com/areas-apis/resume-api/main/docs/spec/_versions/resumeapi-0.1.0.yaml"
 		/>
 	)
 }
