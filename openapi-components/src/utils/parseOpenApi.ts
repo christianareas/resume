80f3a3ce86a9b5d6276f8a3c7b53f2e5462a4679
Christian Areas <me@areas.me>
2023-06-15 15:22:01 -0500

    Manually pointed the OpenAPI parser to the latest OpenAPI version (need to create a better experience).

--- openapi-components/src/utils/parseOpenApi.ts
+++ openapi-components/src/utils/parseOpenApi.ts
@@ -3,7 +3,7 @@ const fs = require("fs")
 const yaml = require("js-yaml")
 
 // Get the OpenAPI object.
-const pathToOpenApiFile = "../../../docs/spec/_versions/resume-api-0.1.0.yaml"
+const pathToOpenApiFile = "../../../docs/spec/_versions/resume-api-0.1.1.yaml"
 const openApiFile = fs.readFileSync(pathToOpenApiFile, "utf8")
 const openApi = yaml.load(openApiFile)
 
