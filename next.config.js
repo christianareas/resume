717e617496dc3e1a12cd3f72fdb4df3d09ea0050
Christian Areas <me@areas.me>
2023-06-14 22:38:27 -0500

    Removed Markdoc from next.config.js file.

--- next.config.js
+++ next.config.js
@@ -7,10 +7,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-// Markdoc.
-const withMarkdoc = require("@markdoc/next.js")
-
-module.exports = withMarkdoc({ schemaPath: "./pages/docs/schema" })({
-  pageExtensions: ["md", "mdoc", "js", "jsx", "ts", "tsx"]
-})
