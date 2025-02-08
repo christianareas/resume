2b1ed905ed56f3ccf738c4c529c518b4662db1e4
Christian Areas <me@areas.me>
2025-02-07 22:45:46 -0800

    Added more details to resume.

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -15,3 +15,230 @@ export const resume = {
 	skills: {},
 	education: {},
 }
+export const resume = {
+	candidate: {
+		candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+		firstName: "Christian",
+		middleName: "",
+		lastName: "Areas",
+		email: "me@areas.me",
+		phoneCountryCode: "1",
+		phoneNumber: "4088029811",
+		website: "areas.me",
+		linkedIn: "https://www.linkedin.com/in/christianareas/",
+		gitHub: "https://github.com/christianareas",
+	},
+	experience: [
+		// Postman
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			experienceId: "637f9a15-ea20-4597-990b-d67bd20db1c1",
+			company: "Postman",
+			role: "Senior Technical Writer",
+			startDate: "2024-03-18",
+			endDate: "",
+			accomplishments: [
+				{
+					candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+					accomplishmentId: "7f4d6d8b-56c4-40a7-9c75-9d48f34a5161",
+					accomplishment:
+						"Started to overhaul the Postman Flows docs. Flows lets you build API-first apps on a drag-and-drop canvas.",
+					sortOrder: 0,
+				},
+				{
+					candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+					accomplishmentId: "5ac5331b-7549-45de-b0a3-64cfbeb46cb8",
+					accomplishment:
+						"Built an icon library Next.js site that lists all of Postman’s icons. The Postman Docs team uses the icons to add visual cues to the docs to improve the user experience.",
+					sortOrder: 1,
+				},
+				{
+					candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+					accomplishmentId: "af61fff1-a32e-43f4-a548-cea6b4181ebc",
+					accomplishment:
+						"Published an npm package that’s a Vale-compatible implementation of the Postman Style Guide. The team uses Vale to lint the docs locally and in GitHub with GitHub Actions — and ultimately ensures the docs adhere to Postman’s style.",
+					sortOrder: 2,
+				},
+			],
+		},
+		// Apple (Special Projects)
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			experienceId: "7b329cf8-5a11-4de8-bdc7-65696dbc1e2c",
+			company: "Apple",
+			role: "Senior Technical Writer, Special Projects",
+			startDate: "2023-09",
+			endDate: "2024-03",
+			accomplishments: [
+				{
+					candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+					accomplishmentId: "26c0fffc-9ce8-49c6-84ff-d404b6bc384d",
+					accomplishment:
+						"Designed a comprehensive workflow that manages the team’s documentation lifecycle. The workflow includes a two-step review process and an automated method to flag outdated content.",
+					sortOrder: 0,
+				},
+				{
+					candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+					accomplishmentId: "28780fdb-38d3-487c-a84b-d05b05f0f5ef",
+					accomplishment:
+						"Worked with cross-functional partners to write how-tos for users who collect data and test in-development products for Apple’s research and development organization.",
+					sortOrder: 1,
+				},
+			],
+		},
+		// Sabbatical
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			experienceId: "3d7d7353-35ef-4be8-b32d-ee8151a13709",
+			company: "Sabbatical",
+			role: "",
+			startDate: "2022-04",
+			endDate: "2023-09",
+			accomplishments: [
+				{
+					candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+					accomplishmentId: "f0814cea-c88c-4f2a-bda4-c9eb779344de",
+					accomplishment:
+						"Completed a 24-week, full-stack JavaScript developer boot camp. Led all team projects — and in one case made over 90% of the contributions to the codebase. Learned TypeScript shortly thereafter.",
+					sortOrder: 0,
+				},
+				{
+					candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+					accomplishmentId: "0ea02570-6c24-45ad-bd6b-fb3816dfb416",
+					accomplishment:
+						"Finished a 5-course, self-paced Python specialization.",
+					sortOrder: 1,
+				},
+			],
+		},
+		// Apple (Apple Pay)
+		// Apple (Employee Productivity)
+		// MGT
+	],
+	skills: [
+		// Languages
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "6d07e824-66d5-4337-9398-628f284e610f",
+			skillType: "languages",
+			skill: "Bash",
+			sortOrder: 0,
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "aa5ac7c3-82d0-4c64-9f84-eb4315c70558",
+			skillType: "languages",
+			skill: "JavaScript and TypeScript",
+			sortOrder: 1,
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "54453dab-4401-4f1a-bc51-45bb92164e40",
+			skillType: "languages",
+			skill: "JSON",
+			sortOrder: 2,
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "7cead30f-24ac-4b67-9d2c-283f6f26bd97",
+			skillType: "languages",
+			skill: "Markdown",
+			sortOrder: 3,
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "314df7bb-fa03-4a93-8802-29cca9db0989",
+			skillType: "languages",
+			skill: "Python",
+			sortOrder: 4,
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "e02050b4-9920-466f-a85c-4bd09409f36f",
+			skillType: "languages",
+			skill: "Regex",
+			sortOrder: 5,
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "3fb48ca5-c5b7-4eff-86aa-fe35649c9365",
+			skillType: "languages",
+			skill: "SQL",
+			sortOrder: 6,
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "e1a28074-7f04-4ded-acf1-d26bcff75376",
+			skillType: "languages",
+			skill: "YAML",
+			sortOrder: 7,
+		},
+		// Specifications
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "4044c487-dafa-46d3-b292-7ed31f0773b2",
+			skillType: "specifications",
+			skill: "GraphQL",
+			sortOrder: 0,
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "0f0e15de-f24d-4eb4-ae3b-e3d9aba3dae7",
+			skillType: "specifications",
+			skill: "gRPC",
+			sortOrder: 1,
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "8146b1b7-7bef-48f8-9cee-9dfacea111ad",
+			skillType: "specifications",
+			skill: "JWT",
+			sortOrder: 2,
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			skillId: "942f9e68-d2c4-409b-aacc-eebace3b5c27",
+			skillType: "specifications",
+			skill: "OpenAPI",
+			sortOrder: 3,
+		},
+		// Command-line tools
+		// Databases
+		// Frameworks
+		// Apps
+	],
+	education: [
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			educationId: "c560a09f-e1c0-438b-bdee-40b08193aa71",
+			institution: "University of Michigan (Coursera)",
+			degree: "Specialization Certificate, Python for Everyone",
+			startDate: "",
+			endDate: "",
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			educationId: "6d2a04b3-3aae-48fe-a96a-95bbc0072d0f",
+			institution: "UC Berkeley Extension",
+			degree: "Certificate of Completion, Full-Stack JavaScript Developer",
+			startDate: "",
+			endDate: "",
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			educationId: "424c3311-d868-4dae-94be-178bf12602d6",
+			institution: "Emory University",
+			degree: "MPH, Epidemiology (Coursework Complete)",
+			startDate: "",
+			endDate: "",
+		},
+		{
+			candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
+			educationId: "96375c83-3995-48b7-a118-131572b154f3",
+			institution: "UC Berkeley",
+			degree: "BA, English and Premed",
+			startDate: "",
+			endDate: "",
+		},
+	],
+}
