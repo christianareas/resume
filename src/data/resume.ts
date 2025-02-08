543a9209e2fd9d7bebc8cd0e46a2d520a0584eb2
Christian Areas <me@areas.me>
2025-02-07 22:49:06 -0800

    Fixed resume.ts file.

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -1,22 +1,5 @@
 export const resume = {
 	candidate: {
-		id: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
-		firstName: "Christian",
-		middleName: "",
-		lastName: "Areas",
-		email: "me@areas.me",
-		phoneCountryCode: "1",
-		phoneNumber: "4088029811",
-		website: "areas.me",
-		linkedIn: "https://www.linkedin.com/in/christianareas/",
-		gitHub: "https://github.com/christianareas",
-	},
-	experience: {},
-	skills: {},
-	education: {},
-}
-export const resume = {
-	candidate: {
 		candidateId: "d5a5e5dc-f2dd-4f5a-8745-0e835d9f26a5",
 		firstName: "Christian",
 		middleName: "",
