8a0b94195cac873985bc5b67bab6d42a4a9964dd
Christian Areas <me@areas.me>
2025-08-03 20:23:11 -0700

    Added API to generate a downloadable PDF of my resume.

---
+++ src/app/api/resume/[candidateId]/pdf/route.ts
@@ -0,0 +1,48 @@
+// Dependencies.
+import { type NextRequest, NextResponse } from "next/server"
+import { chromium } from "playwright-chromium"
+
+// Runtime.
+export const runtime = "nodejs"
+
+// GET request.
+export async function GET(request: NextRequest) {
+	// Create a headless Chromium browser.
+	const browser = await chromium.launch({
+		headless: true,
+		args: ["--no-sandbox", "--disable-setuid-sandbox"],
+	})
+	const browserContext = await browser.newContext({
+		viewport: { width: 1280, height: 900 },
+	})
+
+	// Open the resume page.
+	const resumePage = await browserContext.newPage()
+	await resumePage.emulateMedia({ media: "screen" })
+	const resumeUrl = new URL("/", request.url).toString()
+	await resumePage.goto(resumeUrl, { waitUntil: "networkidle" })
+
+	// Render to PDF.
+	const pdf = await resumePage.pdf({
+		format: "A3",
+		margin: {
+			top: "0",
+			bottom: "0",
+			left: "0",
+			right: "0",
+		},
+		scale: 0.9,
+		printBackground: true,
+	})
+
+	// Close the browser.
+	await browser.close()
+
+	// Return the PDF.
+	return new NextResponse(pdf, {
+		headers: {
+			"Content-Type": "application/pdf",
+			"Content-Disposition": 'attachment; filename="Christian Areas.pdf"',
+		},
+	})
+}
