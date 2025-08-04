715c33b7bc874a790fc2f5fdc22950d0db6ce612
Christian Areas <me@areas.me>
2025-08-04 00:29:11 -0700

    Switched from playwright to puppeteer.

--- src/app/api/resume/[candidateId]/pdf/route.ts
+++ src/app/api/resume/[candidateId]/pdf/route.ts
@@ -1,53 +1,44 @@
 // Dependencies.
 import { type NextRequest, NextResponse } from "next/server"
-import { chromium } from "playwright-chromium"
+import puppeteer from "puppeteer"
 
 // Runtime.
 export const runtime = "nodejs"
 
 // GET request.
 export async function GET(request: NextRequest) {
-	// Create a headless Chromium browser.
-	const browser = await chromium.launch({
-		headless: true,
+	// Create a headless browser.
+	const browser = await puppeteer.launch({
 		args: ["--no-sandbox", "--disable-setuid-sandbox"],
-	})
-	const browserContext = await browser.newContext({
-		viewport: { width: 1280, height: 900 },
+		headless: true,
 	})
 
 	// Open the resume page.
-	const resumePage = await browserContext.newPage()
-	await resumePage.emulateMedia({ media: "screen" })
+	const resumePage = await browser.newPage()
+	await resumePage.setViewport({ width: 1280, height: 900 })
+	await resumePage.emulateMediaType("screen")
 	const resumeUrl = new URL("/", request.url).toString()
-	await resumePage.goto(resumeUrl, { waitUntil: "networkidle" })
+	await resumePage.goto(resumeUrl, { waitUntil: "networkidle0" })
 	await resumePage.evaluate(() => {
-		const downloadButton = document.getElementById("download-button")
-		if (downloadButton) downloadButton.remove()
+		document.getElementById("download-button")?.remove()
 	})
 
 	// Render to PDF.
 	const pdfBuffer = await resumePage.pdf({
 		format: "A3",
-		margin: {
-			top: "0",
-			bottom: "0",
-			left: "0",
-			right: "0",
-		},
-		scale: 0.9,
 		printBackground: true,
+		margin: { top: "0", bottom: "0", left: "0", right: "0" },
+		scale: 0.9,
 	})
-	const pdf = new Uint8Array(pdfBuffer)
 
 	// Close the browser.
 	await browser.close()
 
 	// Return the PDF.
-	return new NextResponse(pdf, {
+	return new NextResponse(new Uint8Array(pdfBuffer), {
 		headers: {
 			"Content-Type": "application/pdf",
-			"Content-Disposition": 'attachment; filename="Christian Areas.pdf"',
+			"Content-Disposition": `attachment; filename="Christian Areas.pdf"`,
 		},
 	})
 }
