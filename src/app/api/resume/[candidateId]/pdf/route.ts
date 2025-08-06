18f1304c03257c8320ea95b9e7afada8122b5b4f
Christian Areas <me@areas.me>
2025-08-05 21:20:14 -0700

    Added logic to either generate the PDF or get it, depending on the environment.

--- src/app/api/resume/[candidateId]/pdf/route.ts
+++ src/app/api/resume/[candidateId]/pdf/route.ts
@@ -9,57 +9,75 @@ export const runtime = "nodejs"
 
 // GET request.
 export async function GET(request: NextRequest) {
-	// Create a headless browser.
-	const browser = await chromium.launch({
-		headless: true,
-		args: ["--no-sandbox", "--disable-setuid-sandbox"],
-	})
-	const browserContext = await browser.newContext({
-		viewport: { width: 1280, height: 900 },
-	})
+	// Vercel environment.
+	const vercelEnvironment = process.env.VERCEL_ENV
 
-	// Open the resume page.
-	const resumePage = await browserContext.newPage()
-	await resumePage.emulateMedia({ media: "screen" })
-	const resumeUrl = new URL("/", request.url).toString()
-	await resumePage.goto(resumeUrl, { waitUntil: "networkidle" })
-	await resumePage.evaluate(() => {
-		const downloadButton = document.getElementById("download-button")
-		if (downloadButton) downloadButton.remove()
-	})
-
-	// Generate the PDF.
-	const pdfBuffer = await resumePage.pdf({
-		format: "A3",
-		margin: {
-			top: "0",
-			bottom: "0",
-			left: "0",
-			right: "0",
-		},
-		scale: 0.9,
-		printBackground: true,
-	})
-	const pdf = new Uint8Array(pdfBuffer)
-
-	// Close the browser.
-	await browser.close()
-
-	// Save the PDF to public/resume/.
-	const publicPath = path.join(
+	// PDF name and location.
+	const pdfName = "Christian Areas.pdf"
+	const pathToPublicDirectory = path.join(
 		process.cwd(),
 		"public",
 		"resume",
-		"Christian Areas.pdf",
+		pdfName,
 	)
-	await fs.mkdir(path.dirname(publicPath), { recursive: true })
-	await fs.writeFile(publicPath, pdf)
 
-	// Return the PDF.
-	return new NextResponse(pdf, {
-		headers: {
-			"Content-Type": "application/pdf",
-			"Content-Disposition": 'attachment; filename="Christian Areas.pdf"',
-		},
-	})
+	// If not in a Vercel environment, generate the PDF.
+	if (vercelEnvironment === undefined || vercelEnvironment === "development") {
+		// Create a headless browser.
+		const browser = await chromium.launch({
+			headless: true,
+			args: ["--no-sandbox", "--disable-setuid-sandbox"],
+		})
+		const browserContext = await browser.newContext({
+			viewport: { width: 1280, height: 900 },
+		})
+
+		// Open the resume page.
+		const resumePage = await browserContext.newPage()
+		await resumePage.emulateMedia({ media: "screen" })
+		const resumeUrl = new URL("/", request.url).toString()
+		await resumePage.goto(resumeUrl, { waitUntil: "networkidle" })
+		await resumePage.evaluate(() => {
+			const downloadButton = document.getElementById("download-button")
+			if (downloadButton) downloadButton.remove()
+		})
+
+		// Generate the PDF.
+		const pdfBuffer = await resumePage.pdf({
+			format: "A3",
+			margin: {
+				top: "0",
+				bottom: "0",
+				left: "0",
+				right: "0",
+			},
+			scale: 0.9,
+			printBackground: true,
+		})
+		const pdf = new Uint8Array(pdfBuffer)
+
+		// Close the browser.
+		await browser.close()
+
+		// Save the PDF to public/resume/.
+		await fs.writeFile(pathToPublicDirectory, pdf)
+
+		// Return the PDF.
+		return new NextResponse(pdf, {
+			headers: {
+				"Content-Type": "application/pdf",
+				"Content-Disposition": `attachment; filename="${pdfName}"`,
+			},
+		})
+
+		// Otherwise, get the PDF from public/resume/.
+	} else {
+		const pdf = await fs.readFile(pathToPublicDirectory)
+		return new NextResponse(pdf, {
+			headers: {
+				"Content-Type": "application/pdf",
+				"Content-Disposition": `attachment; filename="${pdfName}"`,
+			},
+		})
+	}
 }
