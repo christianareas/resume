187751e1fdc64a348481299d6475019e16574e23
Christian Areas <me@areas.me>
2025-08-10 00:01:25 -0700

    Simplified the MCP server route.

--- src/app/api/resume/mcp/route.ts
+++ src/app/api/resume/mcp/route.ts
@@ -6,7 +6,7 @@ import { resume } from "@/data/resume"
 const mcpServer = createMcpHandler(
 	(server) => {
 		// Candidate, experience, skill sets, and education.
-		const { candidate, experience, skillSets, education } = resume
+		const { candidate } = resume
 
 		const resourcesAndTools = [
 			// Resume.
@@ -17,43 +17,6 @@ const mcpServer = createMcpHandler(
 					"The candidate’s resume, including their who, contact details, experience, skill sets, and education.",
 				uri: `resume://${candidate?.candidateId}`,
 				get: () => resume,
-			},
-
-			// Candidate.
-			{
-				title: "Candidate",
-				name: "candidate",
-				description:
-					"The candidate’s details, including first and last name, email, phone number, LinkedIn, and GitHub.",
-				uri: `resume://${candidate?.candidateId}/candidate`,
-				get: () => candidate,
-			},
-
-			// Experience.
-			{
-				title: "Experience",
-				name: "experience",
-				description: "The candidate’s experience, including....",
-				uri: `resume://${candidate?.candidateId}/experience`,
-				get: () => experience,
-			},
-
-			// Skill Sets.
-			{
-				title: "Skill Sets",
-				name: "skillSets",
-				description: "The candidate’s skill sets, including....",
-				uri: `resume://${candidate?.candidateId}/skillSets`,
-				get: () => skillSets,
-			},
-
-			// Education.
-			{
-				title: "Education",
-				name: "education",
-				description: "The candidate’s education, including....",
-				uri: `resume://${candidate?.candidateId}/education`,
-				get: () => education,
 			},
 		]
 
